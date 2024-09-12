ac38bd5ece0125be2a8be2c9ccae74066a1dec13
aldinalj <aldinalj@gmail.com>
2024-09-12 13:21:12 +0200

    Update WeatherWebClientConfig

--- src/main/java/com/aldinalj/weather_web_service/config/WeatherWebClientConfig.java
+++ src/main/java/com/aldinalj/weather_web_service/config/WeatherWebClientConfig.java
@@ -1,4 +1,15 @@
 package com.aldinalj.weather_web_service.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
 public class WeatherWebClientConfig {
+
+    @Bean
+    public WebClient.Builder weatherWebClientBuilder() {
+
+        return WebClient.builder();
+    }
 }
