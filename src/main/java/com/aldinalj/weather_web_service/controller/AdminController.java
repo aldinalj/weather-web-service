7fc161ce7106178c0e7b010ab834f60be3484f6b
aldinalj <aldinalj@gmail.com>
2024-09-14 15:40:17 +0200

    Add Activity entity and Admincontroller class

---
+++ src/main/java/com/aldinalj/weather_web_service/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.aldinalj.weather_web_service.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+}
---
+++ src/main/java/com/aldinalj/weather_web_service/model/Activity.java
@@ -0,0 +1,33 @@
+package com.aldinalj.weather_web_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Activity() {}
+
+    public Activity(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
