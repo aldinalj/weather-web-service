7dd72c2b500f25847a6478f4fa838b9a7c953c0d
aldinalj <aldinalj@gmail.com>
2024-09-14 16:48:52 +0200

    Complete activity entity

--- src/main/java/com/aldinalj/weather_web_service/model/Activity.java
+++ src/main/java/com/aldinalj/weather_web_service/model/Activity.java
@@ -12,12 +12,14 @@ public class Activity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private int code;
 
 
     public Activity() {}
 
-    public Activity(String name) {
+    public Activity(String name, int code) {
         this.name = name;
+        this.code = code;
     }
 
     public Long getId() {
@@ -30,5 +32,13 @@ public class Activity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
     }
 }
