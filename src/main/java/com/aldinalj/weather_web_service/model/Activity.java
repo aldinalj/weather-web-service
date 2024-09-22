b5caabd8a3fc86da071f18c4dc473d3ebc37ff48
aldinalj <aldinalj@gmail.com>
2024-09-22 14:24:52 +0200

    Add validation to all entity attributes

--- src/main/java/com/aldinalj/weather_web_service/model/Activity.java
+++ src/main/java/com/aldinalj/weather_web_service/model/Activity.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -23,23 +25,31 @@ public class Activity {
     @JsonProperty("weather_code")
     private Integer weatherCode;
 
-    @Size(max = 1000)
+    @Size(max = 1000, message = "Description can only be a maximum of 1000 characters")
     private String description;
 
     @JsonProperty("temp_min")
     @Column(name = "temperature_min")
+    @Min(value = -100, message = "Invalid minimum temperature")
+    @Max(value = 50, message = "Invalid maximum temperature")
     private Integer temperatureMin;
 
     @JsonProperty("temp_max")
     @Column(name = "temperature_max")
+    @Min(value = -100, message = "Invalid minimum temperature")
+    @Max(value = 50, message = "Invalid maximum temperature")
     private Integer temperatureMax;
 
     @JsonProperty("price_min")
     @Column(name = "price_min")
+    @Min(value = 0, message = "Minimum price cannot be less than 0")
+    @Max(value = 5000, message = "Minimum price cannot be more than 5000")
     private Integer priceMin;
 
     @JsonProperty("price_max")
     @Column(name = "price_max")
+    @Min(value = 0, message = "Maximum price cannot be less than 0")
+    @Max(value = 5000, message = "Maximum price cannot be more than 5000")
     private Integer priceMax;
 
     public Activity() {}
