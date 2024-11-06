c412763906841a38ba5a24ed1db1fc2ede8ddd89
aldinalj <aldinalj@gmail.com>
2024-11-06 19:18:43 +0100

    refactor project

--- src/main/java/com/aldinalj/weather_web_service/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aldinalj.weather_web_service.user.repository;
-
-import com.aldinalj.weather_web_service.user.model.CustomUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<CustomUser, Long> {
-}
